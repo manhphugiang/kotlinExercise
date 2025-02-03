9cef4ec9f93b3ed21ef1ab2a7b86c1f4e6179ce3
Manh Phu Giang <gmanhphuu@gmail.com>
2025-02-03 14:33:40 -0500

    checkbox ex

---
+++ ex311/app/src/main/java/week3/st991685007manhphugiang/ex311/MainActivity.kt
@@ -0,0 +1,47 @@
+package week3.st991685007manhphugiang.ex311
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import week3.st991685007manhphugiang.ex311.ui.theme.Ex311Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ex311Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ex311Theme {
+        Greeting("Android")
+    }
+}
